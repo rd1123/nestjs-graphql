e6299060f2193fc3f8e61b698290f4ca037d11d6
Nick <rdding1123@gmail.com>
2022-01-11 16:51:14 +0800

    project init and simple graphql sample

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppResolver } from './app.resolver';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      //autoSchemaFile: 'schema.graph'
+    })
+  ],
+  providers: [AppService, AppResolver],
+})
+export class AppModule {}
